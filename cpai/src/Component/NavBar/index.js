4415c3fe68211863fe77795f226414b5f0157fcd
Joey Tsui <joeytsui131@gmail.com>
2024-02-02 23:33:41 -0500

    nav done, will do mobile later

---
+++ cpai/src/Component/NavBar/index.js
@@ -0,0 +1,36 @@
+import "./index.css"
+const NavBar = () => {
+  return (
+    <>
+      <div className="section1">
+        <header className="container">
+          <div className="logo">
+            <img src="https://cpadvancedimaging.com/wp-content/themes/cpai/images/cp-logo-est-1978.jpg"></img>
+          </div>
+
+          <div className="navlink">
+            <ul>
+              <li className="link">
+                <a href="#">Home</a>
+              </li>
+              <li className="link">
+                <a href="#">About</a>
+              </li>
+              <li className="link">
+                <a href="#">Services</a>
+              </li>
+              <li className="link">
+                <a href="#">Physicians</a>
+              </li>
+              <li className="link">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default NavBar
