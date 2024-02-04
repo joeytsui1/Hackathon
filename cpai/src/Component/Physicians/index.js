28c7152647dcb538a24ee57da5127eb34dc6fd03
Joey Tsui <joeytsui131@gmail.com>
2024-02-03 21:12:25 -0500

    push

--- cpai/src/Component/Physicians/index.js
+++ cpai/src/Component/Physicians/index.js
@@ -21,29 +21,84 @@ const Physicians = () => {
                 </a>
               </li>
               <li>
-                <img src="http://cpadvancedimaging.com/wp-content/themes/cpai/images/calvinlo.jpg"></img>
-                <h3>Calvin Lo, M.D.</h3>
-                <h3>Board Certified by the American Board of Radiology</h3>
+                <a href="/physicians/calvin-lo">
+                  <img
+                    src="http://cpadvancedimaging.com/wp-content/themes/cpai/images/calvinlo.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Calvin Lo, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
               </li>
               <li>
-                <img src="http://cpadvancedimaging.com/wp-content/uploads/jchang.jpg"></img>
-                <h3>Calvin Lo, M.D.</h3>
-                <h3>Board Certified by the American Board of Radiology</h3>
+                <a href="/physicians/heather-osbourne">
+                  <img
+                    src="http://cpadvancedimaging.com/wp-content/themes/cpai/images/hosborne.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Heather L. Osborne, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
               </li>
               <li>
-                <img src="http://cpadvancedimaging.com/wp-content/uploads/jchang.jpg"></img>
-                <h3>Calvin Lo, M.D.</h3>
-                <h3>Board Certified by the American Board of Radiology</h3>
+                <a href="/physicians/pouneh-fallahi">
+                  <img
+                    src="http://cpadvancedimaging.com/wp-content/uploads/pfallahi.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Pouneh Fallahi, M.D. MPH</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
               </li>
               <li>
-                <img src="http://cpadvancedimaging.com/wp-content/uploads/jchang.jpg"></img>
-                <h3>Calvin Lo, M.D.</h3>
-                <h3>Board Certified by the American Board of Radiology</h3>
+                <a href="/physicians/sabrina-pieroni">
+                  <img
+                    src="https://cpadvancedimaging.com/wp-content/uploads/2016/08/sabrina-pieronic-website-picture-682x1024.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Sabrina Pieroni, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
               </li>
               <li>
-                <img src="http://cpadvancedimaging.com/wp-content/uploads/jchang.jpg"></img>
-                <h3>Calvin Lo, M.D.</h3>
-                <h3>Board Certified by the American Board of Radiology</h3>
+                <a href="/physicians/amy-law">
+                  <img
+                    src="https://cpadvancedimaging.com/wp-content/uploads/2019/11/amy-law-website-pic-683x1024.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Amy S. Law, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
+              </li>
+              <li>
+                <a href="/physicians/robert-lautin">
+                  <img
+                    src="http://cpadvancedimaging.com/wp-content/themes/cpai/images/rlautin.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Robert F. Lautin, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
+              </li>
+              <li>
+                <a href="/physicians/hari-nandu">
+                  <img
+                    src="https://cpadvancedimaging.com/wp-content/uploads/2019/11/hari-nandu-website-pic-683x1024.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Hari Nandu, M.D</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
+              </li>
+              <li>
+                <a href="/physicians/lawrence-cabusora">
+                  <img
+                    src="https://cpadvancedimaging.com/wp-content/uploads/2016/08/lawrence-cabusora-website-picture-682x1024.jpg"
+                    alt="James Chang"
+                  />
+                  <h3>Lawrence Cabusora, M.D.</h3>
+                  <p>Board Certified by the American Board of Radiology</p>
+                </a>
               </li>
             </ul>
           </div>
